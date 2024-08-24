5687299c5400221038bf7d2b094a36a6603fc003
Orion <Orion45.dd@gmail.com>
2024-08-24 22:00:09 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,5 @@
-SELECT
-	band_name,
-	CASE
-		WHEN split IS NULL 2022 - formed
-		ELSE split - formed
-	END AS lifespan
-FROM
-	metal_bands
-WHERE
-	Style = 'GLAM ROCK'
-ORDER BY
-	lifespan DESC;
+SELECT band_name, 
+IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'GLAM ROCK'
+ORDER BY lifespan DESC;
