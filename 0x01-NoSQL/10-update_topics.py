38f52aa21fb1adb4b31c38422f9037229a95777c
Orion <Orion45.dd@gmail.com>
2024-08-27 15:34:17 -0700

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This script changes all topics of a school document based on the name:"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """The document(object) in the collection will update
+    the name in the list"""
+    temp_name = {"name": name}
+    temp_update = {"$set": {"topics": topics}}
+    result = mongo_collection.update_one(temp_query, temp_update)
