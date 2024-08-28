6280ea13e5ced8ab7e0868292592b14a1f477a40
Orion <Orion45.dd@gmail.com>
2024-08-28 17:36:39 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,19 @@ class Cache:
         temp = str(uuid.uuid4())
         self._redis.set(name=temp, value=data)
         return temp
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        """The get method take a key string argument and an
+        optional Callable argument named fn"""
+        return fn(value) if fn and value is not None else value
+
+    def get_str(self, key: str) -> str:
+        """Get_str will automatically parametrize Cache.get with the
+        correct conversion function."""
+        return self.get(key, bytes.decode)
+
+    def get_int(self, key: str) -> int:
+        """Get_int will automatically parametrize Cache.get with the
+        correct conversion function."""
+        return self.get(key, lambda x: int(x.decode('utf-8')))
